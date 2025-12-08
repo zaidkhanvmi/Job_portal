877d67271352479a95614e0f3d5882503382f94e
zaidkhanvmi <zaidkhan.vmi@gmail.com>
2025-12-08 18:29:10 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import AppRouter from './router/AppRouter'
-import JobProvider, { JobContext } from './Context'
+import JobProvider from './Context'
 
 const App = () => {
   return (
